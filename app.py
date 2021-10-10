4a6a6238e76c18a10b6fed9711c6e9ff318dadfe
Anirban Kar Chaudhuri <anirban.karchaudhuri@gmail.com>
2021-10-10 12:01:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
-from sqlalchemy import create_engine
+from sqlalchemy import *
 from nlp_module.Processor import *
 
 engine = create_engine('sqlite:///pynlp.db', echo=False)
